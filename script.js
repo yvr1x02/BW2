ee9d252dcfc9bac4a3cfe56a9567a42492e01d95
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-06-18 09:42:04 +0200

    side bar resposive

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const rightsideBar = document.getElementById("rightBar");
 const hideBtn = document.getElementById("hide");
 const showBtn = document.getElementById("show");
+const topBar = document.getElementById("topBar");
 
-hideBtn.addEventListener("click ", (event) => {
-  rightsideBar.remove();
+hideBtn.addEventListener("click", () => {
+  rightsideBar.style.display = "none";
+  topBar.classList.remove("col-7");
+  topBar.classList.add("col-9");
+});
+
+showBtn.addEventListener("click", () => {
+  rightsideBar.style.display = "block";
+  topBar.classList.remove("col-9");
+  topBar.classList.add("col-7");
 });
