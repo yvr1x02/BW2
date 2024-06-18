43b252c622bd40bff62bf21a26c4295c96ba2932
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-06-18 12:29:25 +0200

    side bar fatta bene

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const rightsideBar = document.getElementById("rightBar");
 const hideBtn = document.getElementById("hide");
 const showBtn = document.getElementById("show");
 const topBar = document.getElementById("topBar");
+const container = document.getElementById("container");
+
+let removedSidebar = null;
 
 hideBtn.addEventListener("click", () => {
-  rightsideBar.style.display = "none";
-  topBar.classList.remove("col-7");
-  topBar.classList.add("col-9");
+  removedSidebar = rightsideBar;
+  rightsideBar.remove();
+  topBar.classList.remove("col-8");
+  topBar.classList.add("col-10");
 });
 
 showBtn.addEventListener("click", () => {
-  rightsideBar.style.display = "block";
-  topBar.classList.remove("col-9");
-  topBar.classList.add("col-7");
+  if (removedSidebar) {
+    container.insertBefore(removedSidebar, topBar.nextSibling);
+    removedSidebar = null;
+  }
+  topBar.classList.remove("col-10");
+  topBar.classList.add("col-8");
 });
