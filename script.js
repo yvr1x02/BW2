c48512053819ce4667c79a5791ad53b2951ab76f
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-06-20 16:07:52 +0200

    versione mobile fatta per artist page

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const hideBtn = document.getElementById("hide");
 const showBtn = document.getElementById("show");
 const topBar = document.getElementById("topBar");
 const container = document.getElementById("container");
+const topLeft = document.getElementById("topLeft");
 
 let removedSidebar = null;
+let removedTopLeft = null;
 
 hideBtn.addEventListener("click", () => {
   removedSidebar = rightsideBar;
@@ -21,3 +23,35 @@ showBtn.addEventListener("click", () => {
   topBar.classList.remove("col-10");
   topBar.classList.add("col-8");
 });
+
+const mediaQuery = window.matchMedia("(max-width: 763px)");
+
+function handleMediaQueryChange(e) {
+  if (e.matches) {
+    if (rightsideBar && !removedSidebar) {
+      removedSidebar = rightsideBar;
+      rightsideBar.remove();
+      topBar.classList.remove("col-8");
+      topBar.classList.add("col-12");
+    }
+    if (topLeft && !removedTopLeft) {
+      removedTopLeft = topLeft;
+      topLeft.remove();
+    }
+  } else {
+    if (removedSidebar) {
+      container.insertBefore(removedSidebar, topBar.nextSibling);
+      removedSidebar = null;
+      topBar.classList.remove("col-12");
+      topBar.classList.add("col-8");
+    }
+    if (removedTopLeft) {
+      container.insertBefore(removedTopLeft, container.firstChild);
+      removedTopLeft = null;
+    }
+  }
+}
+
+mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+handleMediaQueryChange(mediaQuery);
